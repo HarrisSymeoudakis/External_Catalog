50174b33db2f8939ebe6c3003e754f8d9616c304
fedonch <fedonch@gmail.com>
2024-07-18 12:33:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ async function fetchAndDisplayOrders() {
   try {
     // Fetch items data from API
     const response = await fetch(
-      "https://extcatalog-mq-wempe-server.onrender.com/items"
+      "http://localhost:2999/items"
     );
     const data = await response.json();
  console.log("tiii");
